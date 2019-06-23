d24fae0ef15077f037597b4e8f10ffe2eed51004
spradhan <sm459977@dal.ca>
2019-06-23 20:48:37 -0300

    First game Update.

--- app/src/main/java/com/developer/dejavu/MainActivity.java
+++ app/src/main/java/com/developer/dejavu/MainActivity.java
@@ -1,13 +1,32 @@
 package com.developer.dejavu;
 
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static int SPLASH_TIME_OUT = 3000;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        try
+        {
+            this.getSupportActionBar().hide();
+        }
+        catch (NullPointerException e){}
+
         setContentView(R.layout.activity_main);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent homeIntent = new Intent(MainActivity.this, HomeActivity.class);
+                startActivity(homeIntent);
+            }
+        },SPLASH_TIME_OUT);
     }
 }
