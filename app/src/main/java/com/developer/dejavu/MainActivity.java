53b036a26d4e990b12164c2b2c7f6788b286093a
Aakash Patel <aakashpatel379@gmail.com>
2019-07-10 10:15:44 -0300

    -Google Sign in, Signup,Login,Splash, Home

--- app/src/main/java/com/developer/dejavu/MainActivity.java
+++ app/src/main/java/com/developer/dejavu/MainActivity.java
@@ -1,7 +1,8 @@
 package com.developer.dejavu;
 
+import android.content.Intent;
 import android.os.CountDownTimer;
-
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -9,92 +10,177 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
-
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.ApiException;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.games.Games;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
 
-public class MainActivity extends AppCompatActivity {
 
-    private Button mainButton;
-    private Button leaderboard;
-    private TextView scoreView;
-    private TextView timeView;
-    GoogleApiClient apiClient;
+public class MainActivity extends AppCompatActivity implements  View.OnClickListener {
 
-    private int score = 0;
-    private boolean playing = false;
+    SignInButton signInButton;
+    private final static int RC_SIGN_IN =9001;
+    public static final String TAG="GoogleActivity";
+    private TextView mStatusTextView;
+    private TextView mDetailTextView;
+    private GoogleSignInClient mGoogleSignInClient;
+    private FirebaseAuth.AuthStateListener mAuthListener;
+    FirebaseAuth mAuth;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        apiClient = new GoogleApiClient.Builder(this)
-                .addApi(Games.API)
-                .addScope(Games.SCOPE_GAMES)
+        setContentView(R.layout.layout_main2);
+        signInButton = findViewById(R.id.sign_in_button1);
+        signInButton.setSize(SignInButton.SIZE_STANDARD);
+        mStatusTextView = findViewById(R.id.status);
+        mDetailTextView = findViewById(R.id.detail);
+        mAuth = FirebaseAuth.getInstance();
+        findViewById(R.id.sign_in_button1).setOnClickListener(this);
+        findViewById(R.id.signOutButton).setOnClickListener(this);
+        findViewById(R.id.disconnectButton).setOnClickListener(this);
+
+        // Configure Google Sign In
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken("808106934129-53qft8hm7j3utlasrvc808105an63dee.apps.googleusercontent.com")
+                .requestEmail()
                 .build();
-        apiClient.connect();
-        //Log.w("myApp", );
-        //Toast.makeText(this,""+apiClient.getConnectionResult(Games.API),Toast.LENGTH_LONG);
-        mainButton = (Button)findViewById(R.id.main_button);
-        scoreView = (TextView)findViewById(R.id.score_view);
-        timeView = (TextView)findViewById(R.id.time_view);
-        leaderboard = (Button)findViewById(R.id.leaderboard_button);
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+    }
+    private void signOut() {
+        // Firebase sign out
+        mAuth.signOut();
 
-        mainButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // More code goes here
-                if(!playing) {
-                    // The first click
-                    playing = true;
-                    mainButton.setText("Keep Clicking");
-
-                    // Initialize CountDownTimer to 60 seconds
-                    new CountDownTimer(60000, 1000) {
-                        @Override
-                        public void onTick(long millisUntilFinished) {
-                            timeView.setText("Time remaining: " + millisUntilFinished/1000);
-                        }
-
-                        @Override
-                        public void onFinish() {
-                            playing = false;
-                            timeView.setText("Game over");
-                            mainButton.setVisibility(View.GONE);
-
-                            Games.Leaderboards.submitScore(apiClient,
-                                    getString(R.string.leaderboard_game_leaderboard),
-                                    score);
-                        }
-                    }.start();  // Start the timer
-                } else {
-                    // Subsequent clicks
-                    score++;
-                    scoreView.setText("Score: " + score + " points");
-                }
-            }
-        });
-
-        leaderboard.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                showLeaderboard(v);
-            }
-        });
-
-
+        // Google sign out
+        mGoogleSignInClient.signOut().addOnCompleteListener(this,
+                new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        updateUI(null);
+                    }
+                });
     }
 
-    public void showLeaderboard(View v) {
-        startActivityForResult(
-                Games.Leaderboards.getLeaderboardIntent(apiClient,
-                        getString(R.string.leaderboard_game_leaderboard)), 0);
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        updateUI(currentUser);
     }
 
-    public void showAchievements(View v) {
-        startActivityForResult(
-                Games.Achievements
-                        .getAchievementsIntent(apiClient),
-                1
-        );
+    private void signIn() {
+        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
+
+    private void revokeAccess() {
+        // Firebase sign out
+        mAuth.signOut();
+
+        // Google revoke access
+        mGoogleSignInClient.revokeAccess().addOnCompleteListener(this,
+                new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        updateUI(null);
+                    }
+                });
+    }
+
+
+    private void updateUI(FirebaseUser user) {
+        if (user != null) {
+            mStatusTextView.setText(user.getEmail());
+            mDetailTextView.setText(user.getDisplayName());
+
+            findViewById(R.id.sign_in_button).setVisibility(View.GONE);
+            findViewById(R.id.signOutAndDisconnect).setVisibility(View.VISIBLE);
+        } else {
+            mStatusTextView.setText("SignedOut");
+            mDetailTextView.setText(null);
+
+            findViewById(R.id.sign_in_button).setVisibility(View.VISIBLE);
+            findViewById(R.id.signOutAndDisconnect).setVisibility(View.GONE);
+        }
+    }
+
+    private void firebaseAuthWithGoogle(GoogleSignInAccount acct) {
+        Log.d(TAG, "firebaseAuthWithGoogle:" + acct.getId());
+        //showProgressDialog();
+        AuthCredential credential = GoogleAuthProvider.getCredential(acct.getIdToken(), null);
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "signInWithCredential:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            updateUI(user);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "signInWithCredential:failure", task.getException());
+                            Snackbar.make(findViewById(R.id.layour_main2), "Authentication Failed.", Snackbar.LENGTH_SHORT).show();
+                            updateUI(null);
+                        }
+
+//                        hideProgressDialog();
+                    }
+                });
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Result returned from launching the Intent from GoogleSignInClient.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            // The Task returned from this call is always completed, no need to attach
+            // a listener.
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            handleSignInResult(task);
+        }
+    }
+
+    private void handleSignInResult(Task<GoogleSignInAccount> completedTask) {
+        try {
+            GoogleSignInAccount account = completedTask.getResult(ApiException.class);
+            firebaseAuthWithGoogle(account);
+
+        } catch (ApiException e) {
+            // The ApiException status code indicates the detailed failure reason.
+            // Please refer to the GoogleSignInStatusCodes class reference for more information.
+            Log.w(TAG, "signInResult:failed code=" + e.getStatusCode());
+            Toast.makeText(MainActivity.this,"Auth went wrong",Toast.LENGTH_LONG).show();
+            updateUI(null);
+        }
+    }
+
+    @Override
+    public void onClick(View v) {
+        int i = v.getId();
+        if (i == R.id.sign_in_button1) {
+            signIn();
+        } else if (i == R.id.signOutButton) {
+            signOut();
+        } else if (i == R.id.disconnectButton) {
+            revokeAccess();
+        }
     }
 }
+
