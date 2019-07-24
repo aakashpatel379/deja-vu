aaef76a5b7ddc4173e0167dc31d00b6072a8b0a9
spradhan <sm459977@dal.ca>
2019-07-24 11:25:02 -0300

    All Game Logic. 1) Smart Cpu 2) Zoomable Recyler View(Maginification) 3) Two player Game 4) Accelerometer 5) Game fold Animation

--- app/src/main/java/com/developer/dejavu/HomeActivity.java
+++ app/src/main/java/com/developer/dejavu/HomeActivity.java
@@ -1,19 +1,15 @@
 package com.developer.dejavu;
 
 import android.content.Intent;
-import android.media.MediaPlayer;
-import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 
-public class HomeActivity extends AppCompatActivity implements View.OnClickListener{
+public class HomeActivity extends AppCompatActivity {
 
     Button btn;
-   // For sound
-    Button sound;
-    static boolean soundOn = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,10 +18,6 @@ public class HomeActivity extends AppCompatActivity implements View.OnClickListe
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
         btn = (Button) findViewById(R.id.playGame);
-        sound = (Button) findViewById(R.id.volume);
-
-        sound.setOnClickListener(this);
-
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -33,21 +25,20 @@ public class HomeActivity extends AppCompatActivity implements View.OnClickListe
                 startActivity(intent);
             }
         });
-    }
-
-    @Override
-    public void onClick(View v) {
-        if(v.getId() == R.id.volume){
-            if(soundOn == true) {
-                startService(new Intent(this, SoundService.class));
-                soundOn = false;
+        findViewById(R.id.playCpu).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent =new Intent(HomeActivity.this, CpuGame.class);
+                intent.putExtra("mode", "cpu");
+                startActivity(intent);
             }
-            else if(soundOn == false){
-                stopService(new Intent(this, SoundService.class));
-                soundOn = true;
+        });
+        findViewById(R.id.loadSavedGame).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent =new Intent(HomeActivity.this, CpuGame.class);
+                startActivity(intent);
             }
-
-        }
-
+        });
     }
 }
--- app/src/main/java/com/developer/dejavu/ImageAdapter.java
+++ app/src/main/java/com/developer/dejavu/ImageAdapter.java
@@ -1,6 +1,10 @@
 package com.developer.dejavu;
 
 import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.util.ArraySet;
+import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
@@ -9,23 +13,52 @@ import android.widget.GridView;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 
+import com.developer.dejavu.cpu.CardViewHolder;
+import com.developer.dejavu.cpu.OnItemClickListener;
+import com.developer.dejavu.gameplay.Card;
+
+import java.util.List;
 
 
-public class ImageAdapter extends BaseAdapter {
-
+public class ImageAdapter extends RecyclerView.Adapter<CardViewHolder> {
     private Context context;
+    private OnItemClickListener onItemClickListener;
+    private ArraySet<Card> cards;
 
-    public  ImageAdapter(Context context){
+    public  ImageAdapter(Context context, ArraySet<Card> cards){
         this.context = context;
+        this.cards = cards;
     }
+
+    public void setCards(ArraySet<Card> cards) {
+        this.cards = cards;
+        notifyDataSetChanged();
+    }
+
+    public void setOnItemClickListener(OnItemClickListener onItemClickListener) {
+        this.onItemClickListener = onItemClickListener;
+    }
+
+    @NonNull
     @Override
-    public int getCount() {
-         return 28;
+    public CardViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        ImageView imageView = new ImageView(this.context);
+        imageView.setLayoutParams(new GridView.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT,190));
+        imageView.setScaleType(ImageView.ScaleType.FIT_XY);
+        return new CardViewHolder(imageView);
     }
 
     @Override
-    public Object getItem(int position) {
-        return null;
+    public void onBindViewHolder(@NonNull final CardViewHolder cardViewHolder, int i) {
+        ((ImageView) cardViewHolder.itemView).setImageResource(R.drawable.back);
+        cardViewHolder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (onItemClickListener != null) {
+                    onItemClickListener.onItemClick(v, cardViewHolder.getAdapterPosition());
+                }
+            }
+        });
     }
 
     @Override
@@ -34,19 +67,27 @@ public class ImageAdapter extends BaseAdapter {
     }
 
     @Override
-    public View getView(int position, View convertView, ViewGroup parent) {
-        ImageView imageView;
-        if(convertView == null){
-
-            imageView = new ImageView(this.context);
-            //LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(120, 120);
-            //imageView.setLayoutParams(layoutParams);
-            imageView.setLayoutParams(new GridView.LayoutParams(200,270));
-            imageView.setScaleType(ImageView.ScaleType.FIT_XY);
-        }
-        else imageView = (ImageView) convertView;
-        imageView.setImageResource(R.drawable.back);
-        return imageView;
+    public int getItemCount() {
+        return cards.size();
     }
 
+    public Card getCard(int position) {
+        return cards.valueAt(position);
+    }
+
+    public ArraySet<Card> getCards() {
+        return cards;
+    }
+
+    public void remove(int idx1, int idx2) {
+        Log.e("ImageAdapter", "Removing Items: "+cards.valueAt(idx1).toString()+", "+cards.valueAt(idx2).toString());
+        int maxIdx = Math.max(idx1, idx2);
+        int minIdx = Math.min(idx1, idx2);
+        cards.removeAt(maxIdx);
+        notifyItemRemoved(maxIdx);
+        cards.removeAt(minIdx);
+        notifyItemRemoved(minIdx);
+        Log.d("ImageAdapter", "Remaining Data: "+cards);
+//         notifyDataSetChanged();
+    }
 }
